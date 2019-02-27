d818aee98734185952fb486fddb45f3b0afc0a7c
RyanVillenaUCR <rvill028@ucr.edu>
2019-02-26 18:07:28 -0800

    Part 3 attempted, but error - still shows black monkey

--- fragment.glsl
+++ fragment.glsl
@@ -9,6 +9,16 @@ void main()
     vec3 normal = normalize(N);
 
     float intensity = max(dot(L, normal),0.0);
-    gl_FragColor.rgb = intensity * gl_FrontMaterial.diffuse.rgb * gl_LightSource[0].diffuse.rgb + gl_FrontMaterial.ambient.rgb * gl_LightSource[0].ambient.rgb;
-    gl_FragColor.a = 1.0;
+    
+    vec4 tex_color = texture2D(tex, gl_TexCoord[0]);
+    vec3 tex_color_3;
+    for (int i = 0; i < 3; i++)
+        tex_color_3[i] = tex_color[i];
+    
+    //gl_FragColor.rgb = intensity * gl_FrontMaterial.diffuse.rgb * gl_LightSource[0].diffuse.rgb + gl_FrontMaterial.ambient.rgb * gl_LightSource[0].ambient.rgb;
+    
+    gl_FragColor.rgb = intensity * tex_color_3 * gl_LightSource[0].diffuse.rgb + gl_FrontMaterial.ambient.rgb * gl_LightSource[0].ambient.rgb;
+    //gl_FragColor.a = 1.0;
+    gl_FragColor.a = tex_color[3];
+    
 }
